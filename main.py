2cac107e19b05b37e9525af3f23bb055d3c9daf3
Tarun-kumar-singh <tarun.tarunksingh@gmail.com>
2025-12-20 14:43:47 +0530

    refactor main.py to organize imports and initialize database metadata

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from typing import Union
-
 from fastapi import FastAPI
 
+from app.db.database import engine
+from app.models import company
+
+company.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(health.router)
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
