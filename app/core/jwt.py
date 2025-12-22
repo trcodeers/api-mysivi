94db17f72eeb5b5aa968f90606bc30e71e4bf7a9
Tarun-kumar-singh <tarun.tarunksingh@gmail.com>
2025-12-22 12:38:43 +0530

    fix: correct parameter name for JWT encoding in create_access_token function

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -7,5 +7,5 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, JWT_algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
     return encoded_jwt, expire
